414581583a32fde00e1fb89ea114cf26f318eb0c
Noah Shoap <nshoap@vols.utk.edu>
2022-10-01 12:40:08 -0400

    added args

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,21 @@
 
 int main (int argc, char** argv)
 {
-    (void) argc;
-    (void) argv;
+    std::ifstream file;
+
+    if (argc != 2)
+    {
+        std::cerr << "usage: ./qb-stat-grapher file.csv --flags" << std::endl;
+        return -1; 
+    }
+
+    file.open(argv[1]);
+
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open input file" << std::endl;
+        return -1;
+    }
+
     return 0;
 }
