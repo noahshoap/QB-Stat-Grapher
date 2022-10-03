ea0fade049ae578a6ba04b34af5701a6a3a69a67
Noah Shoap <nshoap@vols.utk.edu>
2022-10-03 11:59:47 -0400

    annoying input problems atm

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
+#include <exception>
 #include "Quarterback.hpp"
 
 int main (int argc, char** argv)
 {
     std::ifstream file;
     std::string line;
+    std::string direction;
+    std::string stat;
+    int number_qbs;
+    int i;
     std::vector<Quarterback*> qbs;
+    std::multimap<double, Quarterback*> sorted_qbs;
     Quarterback *tmp;
 
     /* Exit if not enough args */
-    if (argc != 2)
+    if (argc != 5)
     {
-        std::cerr << "usage: ./qb-stat-grapher file.csv --flags" << std::endl;
+        std::cerr << "usage: ./qb-stat-grapher file.csv top|bottom stat NUM_QBS" << std::endl;
         return -1; 
     }
 
@@ -26,6 +33,22 @@ int main (int argc, char** argv)
         return -1;
     }
 
+    try
+    {
+        direction = argv[2];
+        stat = argv[3];
+        number_qbs = std::stod(argv[4]);
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << "Something is wrong with your command line args." << std::endl;
+        return -1;
+    }
+
+    std::cout << direction << ' ';
+    std::cout << stat << ' ';
+    std::cout << number_qbs << std::endl;
     /* Ignore first line */
     std::getline(file, line);
 
@@ -33,12 +56,40 @@ int main (int argc, char** argv)
     while (std::getline(file, line))
     {
         tmp = new Quarterback(line);
+        if (!(tmp->position == "qb" || tmp->position == "QB"))
+        {
+            delete(tmp);
+            continue;
+        }
         qbs.push_back(tmp);
+
+        if (stat == "touchdowns")
+        {
+            sorted_qbs.insert(std::make_pair(tmp->touchdowns, tmp));
+        }
     }
 
-    for (const auto& p : qbs)
+    if (direction == "top")
     {
-        std::cout << p->name << ' ' << p->game_winning_drives << std::endl;
+        i = 0;
+        for (auto it = sorted_qbs.rbegin(); it != sorted_qbs.rend() && i != number_qbs; ++it, ++i)
+        {
+            if (stat == "touchdowns")
+            {
+                std::cout << it->second->name << ' ';
+                std::cout << it->second->team << ' ';
+                std::cout << it->second->age << ' ';
+                std::cout << it->second->position << ' ';
+                std::cout << it->second->games_played << ' ';
+                std::cout << it->second->games_started << ' ';
+                std::cout << it->second->record << ' ';
+                std::cout << it->second->completions << ' ';
+                std::cout << it->second->attempts << ' ';
+                std::cout << it->second->completion_percentage << ' ';
+                std::cout << it->second->yards << ' ';
+                std::cout << it->second->touchdowns << std::endl;
+            }
+        }
     }
 
     return 0;
