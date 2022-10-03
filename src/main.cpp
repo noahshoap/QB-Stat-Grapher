9ed4063edb7d4332edea759377975af3dc9bb745
Noah Shoap <nshoap@vols.utk.edu>
2022-10-03 18:14:23 -0400

    s

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,6 @@ int main (int argc, char** argv)
     int number_qbs;
     int i;
     int max_y;
-    int hash_index;
     double max_x;
     std::vector<Quarterback*> qbs;
     std::multimap<double, Quarterback*> sorted_qbs;
